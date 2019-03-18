20231eefdd124cfbe757fef17a52209efff4824b
abid <abidhussain@pursuit.org>
2019-03-18 11:03:26 -0400

    seeded database with faker

--- backend/db/seed.js
+++ backend/db/seed.js
@@ -3,52 +3,58 @@ const db = require("./connection");
 const faker = require("faker");
 
 let genres = [];
-for (let i = 0; i < 11; i++) {
-  let name = faker.random.words();
-  let str = `('${name}')`;
+for (let i = 0; i < 6; i++) {
+  let genre_name = faker.random.words();
+  let str = `('${genre_name}')`;
   genres.push(str);
 }
 
-let movies = [];
-for (let i = 0; i < 20; i++) {
+let songs = [];
+for (let i = 0; i < 16; i++) {
   let title = faker.lorem.words();
-  let genre_id = Math.floor(Math.random() * 10) + 1;
+  let genre_id = Math.floor(Math.random() * 5) + 1;
+  let user_id = Math.floor(Math.random() * 10) + 1;
   let img_url = faker.image.image();
-  let str = `('${title}', ${genre_id}, '${img_url}')`;
-  movies.push(str);
+  let str = `('${title}', ${genre_id}, ${user_id}, '${img_url}')`;
+  songs.push(str);
 }
 
-let ratings = [];
-for (let i = 0; i < 100; i++) {
-  let stars = Math.floor(Math.random() * 5) + 1;
-  let movie_id = Math.floor(Math.random() * 20) + 1;
-  let str = `(${stars}, ${movie_id})`;
-  ratings.push(str);
+let favorites = [];
+for (let i = 0; i < 41; i++) {
+  let user_id = Math.floor(Math.random() * 10) + 1;
+  let song_id = Math.floor(Math.random() * 15) + 1;
+  let str = `(${user_id}, ${song_id})`;
+  favorites.push(str);
 }
 
 let comments = [];
-for (let i = 0; i < 30; i++) {
-  let body = faker.lorem.paragraph();
-  let movie_id = Math.floor(Math.random() * 20) + 1;
-  let str = `('${body}', ${movie_id})`;
+for (let i = 0; i < 21; i++) {
+  let comment_body = faker.lorem.sentence();
+  let user_id = Math.floor(Math.random() * 10) + 1;
+  let song_id = Math.floor(Math.random() * 15) + 1;
+  let str = `('${comment_body}', ${user_id}, ${song_id})`;
   comments.push(str);
 }
 
 genres = genres.join(", ");
-movies = movies.join(", ");
-ratings = ratings.join(", ");
+songs = songs.join(", ");
+favorites = favorites.join(", ");
 comments = comments.join(", ");
 
-db.none("INSERT INTO genres(name) VALUES " + genres + ";")
+db.none("INSERT INTO genres(genre_name) VALUES " + genres + ";")
   .then(() => {
     db.none(
-      "INSERT INTO movies(title, genre_id, img_url) VALUES " + movies + ";"
+      "INSERT INTO songs(title, genre_id, user_id, img_url) VALUES " +
+        songs +
+        ";"
     ).then(() => {
       db.none(
-        "INSERT INTO ratings(stars, movie_id) VALUES " + ratings + ";"
+        "INSERT INTO favorites(user_id, song_id) VALUES " + favorites + ";"
       ).then(() => {
         db.none(
-          "INSERT INTO comments(body, movie_id) VALUES " + comments + ";"
+          "INSERT INTO comments(comment_body, user_id, song_id) VALUES " +
+            comments +
+            ";"
         );
       });
     });
